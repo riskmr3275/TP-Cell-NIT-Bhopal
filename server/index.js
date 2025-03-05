8a20d239cab650eb3a34248f92515981a5667b2a
Risu Gupta <riskmr3275@gmail.com>
2025-03-06 00:22:07 +0530

    Add

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(cookieParser());
  
 app.use(cors({
-    origin:"http://localhost:3000",
+    origin:"https://tp-cell-nit-bhopal-india-re1mmimgh-risu-guptas-projects.vercel.app/",
     credentials:true,
 }))
 
